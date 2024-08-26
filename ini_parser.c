7812ea9626901ad3f1339eb318e64258a23f99cd
bvhoang4285 <148993403+bvhoang4285@users.noreply.github.com>
2024-08-26 22:55:53 +0700

    Update ini_parser.c

--- ini_parser.c
+++ ini_parser.c
@@ -3,59 +3,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void idb_init_group(idb_group* pgrp, unsigned int nkeys, char* name, unsigned int keylen)
+void idb_init_group(idb_group* pgrp, unsigned int nkeys, char* name, unsigned int namelen)
 {
 	pgrp->n_keys= 0;
 	pgrp->n_keys_init= nkeys;
-	pgrp->p_keys= (idb_key*) malloc(sizeof(idb_key)*nkeys);
-	pgrp->name= (char*)malloc((keylen+1)*sizeof(char));
-	strcpy(pgrp->name, name);
-	pgrp->name[keylen+1]= 0;
+	if(nkeys > 0)
+	{
+		pgrp->p_keys= (idb_key*)malloc(sizeof(idb_key)*nkeys);
+	}
+	pgrp->name= (char*)malloc((namelen+1)*sizeof(char));
+	memcpy(pgrp->name, name, namelen);
+	pgrp->name[namelen]= 0;
 }
 
 void idb_reset_group(idb_group* pgrp)
 {
-	pgrp->n_keys= 0;
+	int i;
+
+	if(pgrp->name)
+	{
+		free(pgrp->name);
+		pgrp->name= NULL;
+	}
+
+	for(i= 0; i < pgrp->n_keys; i++)
+	{
+		free(pgrp->p_keys[i].name);
+		free(pgrp->p_keys[i].val);
+	}
+
 	if(pgrp->p_keys)
 	{
 		free(pgrp->p_keys);
+		pgrp->p_keys= NULL;
 	}
-	pgrp->p_keys= NULL;
+	pgrp->n_keys= 0;
 }
 
 void idb_set_key(idb_key* pkey, char* name, char* val)
 {
-	unsigned int len, i, j;
+	int len, i, j;
 	len= strlen(name);
 	if(len > 0)
 	{
 		for(i= 0; i < len; i++) if(name[i] > ' ') break;
 		for(j= len-1; j > 0; j--) if(name[j] > ' ') break;
-		pkey->name= (char*) malloc((j+1)*sizeof(char));
-		strncpy(pkey->name, &name[i], j+1);
-		pkey->name[j+1]= 0;
+		pkey->name= (char*) malloc((j-i+2)*sizeof(char));
+		memcpy(pkey->name, &name[i], j-i+2);
+		pkey->name[j-i+1]= 0;
 	}
 	len= strlen(val);
 	if(len > 0)
 	{
 		for(i= 0; i < len; i++) if(val[i] > ' ') break;
 		for(j= len-1; j > 0; j--) if(val[j] > ' ') break;
-		pkey->val= (char*) malloc((j+1)*sizeof(char));
-		strncpy(pkey->val, &val[i], j+1);
-		pkey->val[j+1]= 0;
+		pkey->val= (char*) malloc((j-i+2)*sizeof(char));
+		memcpy(pkey->val, &val[i], j-i+2);
+		pkey->val[j-i+1]= 0;
 	}
 }
 
-void idb_add_key(idb_group* pgrp, idb_key* pkey)
+void idb_add_key(idb_group* pgrp)
 {
 	idb_key* pnew;
+	if(pgrp->n_keys == 0)
+	{
+		pgrp->p_keys= (idb_key*) malloc(sizeof(idb_key));
+	}
+	else
 	if(pgrp->n_keys >= pgrp->n_keys_init)
 	{
 		pnew= (idb_key*) realloc(pgrp->p_keys, sizeof(idb_key)*(pgrp->n_keys+1));
 		pgrp->p_keys= pnew;
 	}
 
-	memcpy((void*)&pgrp->p_keys[pgrp->n_keys], (void*)pkey, sizeof(idb_key));
 	pgrp->n_keys+= 1;
 }
 
@@ -63,7 +84,10 @@ void idb_init(idb_data *pdb, unsigned int n_grp)
 {
 	pdb->n_groups= 0;
 	pdb->n_groups_init= n_grp;
-	pdb->p_groups= (idb_group*) malloc(sizeof(idb_group)*n_grp);
+	if(n_grp > 0)
+	{
+		pdb->p_groups= (idb_group*) malloc(sizeof(idb_group)*n_grp);
+	}
 }
 
 void idb_reset(idb_data *pdb)
@@ -74,38 +98,43 @@ void idb_reset(idb_data *pdb)
 	{
 		idb_reset_group(&pdb->p_groups[i]);
 	}
+
+	if(pdb->p_groups)
+	{
+		free(pdb->p_groups);
+	}
 	pdb->n_groups= 0;
+	pdb->n_groups_init= 0;
 	pdb->p_groups= NULL;
-	free(pdb->p_groups);
 }
 
-void idb_add_group(idb_data *pdb, idb_group* pgr)
+void idb_add_group(idb_data *pdb)
 {
 	idb_group* pnew;
+	if(pdb->n_groups == 0)
+	{
+		pdb->p_groups= (idb_group*) malloc(sizeof(idb_group));
+	}
+	else
 	if(pdb->n_groups >= pdb->n_groups_init)
 	{
 		pnew= (idb_group*) realloc(pdb->p_groups, sizeof(idb_group)*(pdb->n_groups+1));
 		pdb->p_groups= pnew;
 	}
 
-	memcpy((void*)&pdb->p_groups[pdb->n_groups], (void*)pgr, sizeof(idb_group));
 	pdb->n_groups+= 1;
 }
-
-
 
 void idb_from_file(idb_data* pdb, char* path)
 {
 	FILE* pfile;
-	char buf[1024];
-	char name[512];
-	char val[512];
+	char buf[1280];
+	char name[128];
+	char val[1152];
 	char dummy;
 	int i, ret, len;
 	idb_group* pnew_group;
 	idb_key* pnew_key;
-
-	idb_init(pdb, 1);
 
 	pfile= fopen(path, "rt");
 	if(!pfile)
@@ -128,18 +157,17 @@ void idb_from_file(idb_data* pdb, char* path)
 		if(buf[0] == '[' && buf[len-1] == ']')
 		{
 			buf[len-1]= 0;
-			strcpy(name, &buf[1]);
-			pnew_group= (idb_group*) malloc(sizeof(idb_group));
-			idb_init_group(pnew_group, 1, name, len);
-			idb_add_group(pdb, pnew_group);
+			idb_add_group(pdb);
+			pnew_group= &pdb->p_groups[pdb->n_groups-1];
+			idb_init_group(pnew_group, 0, &buf[1], len-2);
 		}
 		else
 		if(buf[0] != ';')
 		{
 			sscanf(buf, " %[^=] %c %[^\r^\n]", name, &dummy, val);
-			pnew_key= (idb_key*) malloc(sizeof(idb_key));
+			idb_add_key(pnew_group);
+			pnew_key= &pnew_group->p_keys[pnew_group->n_keys-1];
 			idb_set_key(pnew_key, name, val);
-			idb_add_key(&pdb->p_groups[pdb->n_groups-1], pnew_key);
 		}
 	}
 }
